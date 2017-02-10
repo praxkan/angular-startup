3e6c8866ab88f97eeddda9a13efaa827091e5c73
Prax <praxkan@gmail.com>
2017-02-09 22:08:57 -0500

    first

---
+++ build-scripts/demo-webserver.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Prax on 09/02/2017.
+ */
+'use strict';
+
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 3000;
+const app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function (err) {
+  if(err) {
+    console.log(err);
+  } else {
+    open('http://localhost:'+port);
+  }
+});
